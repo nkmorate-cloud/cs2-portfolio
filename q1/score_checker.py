390c6f9cb2716ff59627886b69f37e32ac19d8c5
nkmorate-cloud <nkmorate@brc.pshs.edu.ph>
2026-09-06 19:53:32 +0800

    Update score_checker.py

--- q1/score_checker.py
+++ q1/score_checker.py
@@ -1,1 +1,12 @@
-
+score = int(input("Enter student score: "))
+if score < 0 or score > 100:
+  print("Invalid score.")
+  exit()
+if 90 <= score <= 100:
+    print("Outstanding")
+elif 80 <= score <= 89:
+    print("Very Satisfactory")
+elif 75 <= score <= 79:
+    print("Satisfactory")
+else:
+    print("Needs Improvement")
